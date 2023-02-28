34c33794cebdb55a73100c75415d09ace781d5af
LarissaSimoes <larissacarneiro2@hotmail.com>
2023-02-28 09:43:52 -0300

    Req 3 modificado

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import { FavoritePokemon } from '../pages';
-import pokemonList from '../data';
+import App from '../App';
 
 describe('Testes do componente FavoritePokemon', () => {
   it('Testando se se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
@@ -12,10 +13,14 @@ describe('Testes do componente FavoritePokemon', () => {
   });
 
   it('Testando se apenas são exibidos os Pokémon favoritados', () => {
-    renderWithRouter(<FavoritePokemon pokemonList={ pokemonList } />);
-    pokemonList.forEach((pokemon) => {
-      const name = screen.getByText(pokemon.name);
-      expect(name).toBeInTheDocument();
-    });
+    renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(detailsLink);
+    const checkBoxFavorite = screen.getByRole('checkbox');
+    userEvent.click(checkBoxFavorite);
+    const favoritePokemon = screen.getByRole('link', { name: 'Favorite Pokémon' });
+    userEvent.click(favoritePokemon);
+    const message = screen.getByText('Pikachu');
+    expect(message).toBeInTheDocument();
   });
 });
